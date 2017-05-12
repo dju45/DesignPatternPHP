20e2b6db6fabe09a64c59bf3ba4c25eb4c2953ca
Tony133 <a.tripodi133@gmail.com>
2017-05-12 18:38:28 +0200

    Add Proxy Test

---
+++ tests/ProxyTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use PHPUnit\Framework\TestCase;
+use App\Proxy\Security;
+use App\Proxy\LabDoor;
+
+class Proxy extends TestCase
+{
+    public function testSecurityLabDoor()
+    {
+    	$door = new Security(new LabDoor());
+		$expectedObject1 = $door->open('invalid'); // Big no! It ain't possible.
+		$expectedObject2 = $door->open('$ecr@t'); // Opening lab door
+		$expectedObject3 = $door->close(); // Closing lab door
+
+		$mock = $this->getMockBuilder(Security::class)
+                     ->disableOriginalConstructor()
+                     ->setMethods(['open'])
+                     ->getMock();
+
+        $mock->expects($this->any())
+             ->method('open')
+             ->will($this->returnValue('Big no! It ain\'t possible.'))
+             ->with($this->identicalTo($expectedObject1));
+
+        $mock->expects($this->any())
+             ->method('open')
+             ->will($this->returnValue('Opening lab door'))
+             ->with($this->identicalTo($expectedObject2));
+
+        $mock->expects($this->any())
+             ->method('open')
+             ->will($this->returnValue('Closing lab door'))
+             ->with($this->identicalTo($expectedObject3));
+    }
+}
+
